6cf4aca8493bff09b36bd87777551616a13a9b84
hanga.lagoue <wilfriedhanga5@gmail.com>
2024-06-14 08:08:31 +0100

    setup

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { fileURLToPath, URL } from 'node:url';
+
+import { defineConfig } from 'vite';
+import solidPlugin from 'vite-plugin-solid';
+import devtools from 'solid-devtools/vite';
+
+export default defineConfig({
+  plugins: [
+    devtools(),
+    solidPlugin(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  server: {
+    port: 3000,
+  },
+  build: {
+    target: 'esnext',
+  },
+});
